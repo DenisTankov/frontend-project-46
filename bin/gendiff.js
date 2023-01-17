d590f6bac180029fbbcdd8bf34aee3dda23154e5
Денис Таньков <denis_tankoff@MacBook-Pro-Denis.local>
2023-01-17 22:25:53 +0700

    added global gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import { Command } from 'commander/esm.mjs';
 
-console.log('вроде получается');
+const program = new Command();
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.');
+program.parse();
