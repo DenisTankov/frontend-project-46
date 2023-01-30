96c9d52c136ee8610bd9b4c0930efa71e8cc9e2b
Денис Таньков <denis_tankoff@mail.ru>
2023-01-30 08:06:02 +0700

    added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
-import { Command } from 'commander/esm.mjs';
+
+import { Command } from 'commander';
+//import diff from '../src/index.js';
 
 const program = new Command();
+
 program
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  //.action((filepath1, filepath2, options) => {
+  //  console.log(diff(filepath1, filepath2, options.format));
+  //});
+
 program.parse();
